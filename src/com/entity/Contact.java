f35d8a686fe2a6ddbc15230080e6ef756ff5e50b
SuhasBhor <80667151+SuhasBhor@users.noreply.github.com>
2022-01-10 20:50:23 +0530

    Initialize Contact Variables

---
+++ src/com/entity/Contact.java
@@ -0,0 +1,14 @@
+package com.entity;
+
+public class Contact {
+	
+	//Initialize variables
+	String firstName;
+	String lastName;
+	String address;
+	String city;
+	String state;
+	int zipCode;
+	long phoneNumber;
+	String email;
+}
